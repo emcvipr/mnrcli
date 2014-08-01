7b64315d4d9c892b8728c61f146367377a344ef7
baua.gonzo <baua.gonzo@gmail.com>
2014-08-01 14:23:44 +0000

    +Options
    git-svn-id: http://w4n-pl-cvs01.int.watch4net.net/svn/Watch4NetRP/_tools/mnrcli@7863 dadf6eda-54ab-0410-8d58-c1c68f033599

--- mnrcli.rb
+++ mnrcli.rb
@@ -6,15 +6,33 @@ $:.unshift 'lib'
 require 'pry'
 require 'bond'
 require 'savon'
+require 'optparse'
 require 'w4n/additions'
 require 'w4n/filter'
 require 'w4n/cli'
-#require 'pry/bond/default'
-#Bond.config[:readline]=Pry::BondCompleter::NoopReadline
-#Bond::M.load_file './completion.rb'
 
 options={host: 'localhost', user: 'admin', password: 'changeme'}
 options.merge!(YAML.load_file('./config.yml').sym_keys)
+
+
+OptionParser.new do |opts|
+  opts.banner = "Usage: mnrcku.rb [options]"
+
+  opts.on("-h", "--host [hostname]", String, "Web-Server host") do |h|
+    options[:host] = h
+  end
+  opts.on("-u", "--user [username]", String, "Web-Service username") do |u|
+    options[:user] = u
+  end
+  opts.on("-p", "--password [*****]", String, "Web-Service password") do |p|
+    options[:password] = p
+  end
+end.parse!
+
+p options
+p ARGV
+
+
 
 [Filter].each do |c|
   c.class_eval "
